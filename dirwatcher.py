19e60c26dd69c3ffe3568c7d97a65c0d62848bab
Jessica Benson <pokeyjess72@gmail.com>
2020-07-21 02:22:58 -0400

    Adds timestamp with logging

--- dirwatcher.py
+++ dirwatcher.py
@@ -14,6 +14,11 @@ shutdown and start up banners
 tests
 flake8, docstrings, etc.
 
+
+when using logging in more than one file per directory
+why? what's going on there?
+AttributeError: partially initialized module 'logging' has no attribute 'getLogger' 
+(most likely due to a circular import)
 '''
 
 import signal
@@ -22,6 +27,11 @@ import logging
 import os
 
 exit_flag = False
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%Y-%m-%d &%H:%M:%S')
+# timestamp
 
 
 def signal_handler(sig_num, frame):
@@ -33,17 +43,17 @@ def signal_handler(sig_num, frame):
     :return None
     """
     global exit_flag
-    print(f"Handling signal {signal.Signals(sig_num).name}")
+    logger.debug(f"Handling signal: {signal.Signals(sig_num).name}")
     exit_flag = True
     # call this function when program gets a signal (see below)
 
     # log the associated signal name
-    # logger.warn('Received ' + signal.Signals(sig_num).name)
-
-# function to watch directory
 
 
-def find_magic_text(filename, last_position, magic_text:
+#  need function to watch directory****
+
+
+def find_magic_text(filename, last_position, magic_text):
     """
    Function to look for magic text
    Searches for magic text in file (from last line position)
@@ -58,6 +68,9 @@ def find_magic_text(filename, last_position, magic_text:
                         f'Magic text: "{magic_text}", found at line: {line_number + 1} in: {filename}')
     return line_number + 1
 
+
+# need argparse
+
 def main():
     # Hook into these two signals from the OS
     signal.signal(signal.SIGINT, signal_handler)
@@ -65,6 +78,8 @@ def main():
     # Now my signal_handler will get called if OS sends
     # either of these to my process.
 
+# change print statements to logging:
+# https://www.youtube.com/watch?v=jxmzY9soFXg, last 5 min
     while not exit_flag:
         try:
             print(f"[{os.getpid()}] Tick...")
@@ -81,7 +96,7 @@ def main():
             pass
 
         # exit banner. make separate function?
-        print("------------\nExiting normally\n------------")
+        print(f"{16 * '-'}\nExiting normally\n{16 * '-'}")
 
         # put a sleep inside my while loop so I don't peg the cpu usage at 100%
         # time.sleep(polling_interval)
